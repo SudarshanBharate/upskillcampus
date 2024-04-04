59e864684eaa5e3bb6e352f32cd80e7ce06103f3
SudarshanBharate <76870710+SudarshanBharate@users.noreply.github.com>
2024-04-04 21:10:01 +0530

    Delete Url-Shortener-main directory

--- Url-Shortener-main/url_shortener_project/urls.py
+++
@@ -1,8 +0,0 @@
-# url_shortener_project/urls.py
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('url_shortener.urls')),
-]
